fef384b75ac9f5d4ce633661c44b37bf8ddb9ff5
spectrox0 <alejanvelazco2008@hotmail.com>
2020-05-24 18:04:04 -0400

    arreglo equis

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,78 +1,60 @@
 const { gql } = require("apollo-server-express");
-const User = require('../../models/users');
+const User = require("../../models/users");
 
 module.exports = gql`
-    type User {
-        _id: ID!
-        name: String!
-        lastName: String!
-        birthdate: String!
-        mail: String!
-        password:String!
-        zone:String!
-        cellphone:String!
-    }
+  type User {
+    _id: ID!
+    name: String!
+    lastName: String!
+    birthdate: String!
+    mail: String!
+    password: String!
+    zone: String!
+    cellphone: String!
+  }
 
-    input UserInput {
-        name: String!
-        lastName: String!
-        birthdate: String!
-        mail: String!
-        password:String!
-        zone:String!
-        cellphone:String!
-    }
+  input UserInput {
+    name: String!
+    lastName: String!
+    birthdate: String!
+    mail: String!
+    password: String!
+    zone: String!
+    cellphone: String!
+  }
 
-    type Repartidor {
-        _id: ID!
-        name: String!
-        lastName: String!
-        birthdate: String!
-        mail: String!
-        password:String!
-        zone:String!
-        cellphone:String!
-        status:String!
-        hiringDate:String!
-    }
+  type Repartidor {
+    _id: ID!
+    name: String!
+    lastName: String!
+    birthdate: String!
+    mail: String!
+    password: String!
+    zone: String!
+    cellphone: String!
+    status: String!
+    hiringDate: String!
+  }
 
-    input RepartidorInput {
-        name: String!
-        lastName: String!
-        birthdate: String!
-        mail: String!
-        password:String!
-        zone:String!
-        cellphone:String!
-        status:String!
-        hiringDate:String!
-    }
+  input RepartidorInput {
+    name: String!
+    lastName: String!
+    birthdate: String!
+    mail: String!
+    password: String!
+    zone: String!
+    cellphone: String!
+    status: String!
+    hiringDate: String!
+  }
 
-// Querys-------------------------------------------------------------------------------------------------------------------
-    
-    type Query {
-        users: [User!]!
-    }
+  type Query {
+    users: [User!]!
+    repartidores: [Repartidor!]!
+  }
 
-    type Query {
-        repartidores: [Repartidor!]!
-    }
-
-// Mutations----------------------------------------------------------------------------------------------------------------
-    
-    type Mutation {
-        createUser(userInput: UserInput): User
-    }
-
-    type Mutation {
-        createRepartidor(repartidorInput: RepartidorInput): Repartidor
-    }
-
-// Schema-------------------------------------------------------------------------------------------------------------------
-
-    schema {
-        query: Query
-        mutation: Mutation
-    }
-    
+  type Mutation {
+    createUser(userInput: UserInput): User
+    createRepartidor(repartidorInput: RepartidorInput): Repartidor
+  }
 `;
