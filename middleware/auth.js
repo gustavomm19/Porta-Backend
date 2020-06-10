a11906f48b161d3dccb488933124aa6163f7bfd1
spectrox0 <alejanvelazco2008@hotmail.com>
2020-06-09 21:29:52 -0400

    mover middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,24 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = async ({ req }) => {
+module.exports = async (req, res, next) => {
   let authToken;
-
   authToken = req.get("Authorization");
-
   if (!authToken) {
-    req.isAuth = false;
-    return;
+    return next();
   }
   const token = authToken.split(" ")[1];
   if (!token || token === "") {
-    return;
+    return next();
   }
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, process.env.CREDENTIALS_JWT);
   } catch (err) {
-    return;
+    return next();
   }
   if (!decodedToken) {
     return;
   }
-
-  return { token: decodedToken };
+  req.token = decodedToken;
+  return next();
 };
