be042790374c1372e83ad932b6854f12a03f81b4
Luis Arturo Castaneda <luis.castaneda@correo.unimet.edu.ve>
2020-07-06 20:09:30 -0400

    ultimo

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const api = express.Router();
 const controllers = require("../controllers");
 
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name:'portaapp',
+    api_key:'563755334298556',
+    api_secret:'YOnANVWzbdNitUBNs_HekDccpCc'
+});
+
 api.post("/uploadImage", controllers.uploadImage);
 
 api.get('/prueba', (req, res) => {
