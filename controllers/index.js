1043ecafba12f8f60c1c1ca7d6620ba7f666483b
Gustavo Marquez <gustavomm19@hotmail.com>
2020-07-18 02:34:12 -0400

    profilepic

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,20 @@
+const User = require('../models/users');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name:'portaapp',
+    api_key:'563755334298556',
+    api_secret:'YOnANVWzbdNitUBNs_HekDccpCc'
+});
+
 exports.uploadImage = async (req, res, next) => {
-  return res.status(200).send({ message: "sube imagen", urlImg: null });
+  const { userId } = req.body;
+  const user = await User.findById(userId);
+
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  user.userImageURL = result.url;
+  user.userImageId = result.public_id;
+
+  const newUser = await user.save();
+
+  return res.status(200).send(newUser);
 };
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const api = express.Router();
 const controllers = require("../controllers");
 
-const cloudinary = require('cloudinary');
-cloudinary.config({
-    cloud_name:'portaapp',
-    api_key:'563755334298556',
-    api_secret:'YOnANVWzbdNitUBNs_HekDccpCc'
-});
+// const cloudinary = require('cloudinary');
+// cloudinary.config({
+//     cloud_name:'portaapp',
+//     api_key:'563755334298556',
+//     api_secret:'YOnANVWzbdNitUBNs_HekDccpCc'
+// });
 
 api.post("/uploadImage", controllers.uploadImage);
 
